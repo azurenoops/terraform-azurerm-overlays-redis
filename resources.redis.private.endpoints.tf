e06245a2624d367b64caef96ff4afaae1f4e6eb5
John Spinella <jrspinella@users.noreply.github.com>
2023-08-09 12:58:36 -0400

    Update to PE

--- resources.redis.private.endpoints.tf
+++ resources.redis.private.endpoints.tf
@@ -67,7 +67,7 @@ resource "azurerm_private_dns_a_record" "a_rec" {
   ]
   count               = var.enable_private_endpoint  ? 1 : 0
   name                = lower(azurerm_redis_cache.redis.name)
-  zone_name           = azurerm_private_dns_zone.dns_zone.0.name
+  zone_name           = var.existing_private_dns_zone == null ? azurerm_private_dns_zone.dns_zone.0.name : var.existing_private_dns_zone
   resource_group_name = local.resource_group_name
   ttl                 = 300
   records             = [data.azurerm_private_endpoint_connection.pip.0.private_service_connection.0.private_ip_address]
